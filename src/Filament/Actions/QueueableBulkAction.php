93cfb7ec84cfb36af63d5a77248ef6f415f243f2
Eddie Rusinskas <erusinskas@wonde.com>
2024-04-22 20:43:13 +0100

    wip

--- src/Filament/Actions/QueueableBulkAction.php
+++ src/Filament/Actions/QueueableBulkAction.php
@@ -3,6 +3,7 @@
 namespace Bytexr\QueueableBulkActions\Filament\Actions;
 
 use Bytexr\QueueableBulkActions\Enums\BulkActions\TypeEnum;
+use Bytexr\QueueableBulkActions\Enums\StatusEnum;
 use Bytexr\QueueableBulkActions\Jobs\BulkActionSetupJob;
 use Bytexr\QueueableBulkActions\Models\BulkAction;
 use Bytexr\QueueableBulkActions\Support\Config;
@@ -11,10 +12,22 @@ use Exception;
 use Filament\Pages\BasePage;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
+use Laravel\SerializableClosure\SerializableClosure;
 
 class QueueableBulkAction extends \Filament\Tables\Actions\BulkAction
 {
-    private Closure | string | null $job = null;
+    private Closure|string|null $job = null;
+
+    public function call(array $parameters = []): mixed
+    {
+        try {
+            return parent::call($parameters);
+        } finally {
+            if ($this->shouldDeselectRecordsAfterCompletion()) {
+                $this->getLivewire()->deselectAllTableRecords();
+            }
+        }
+    }
 
     protected function setUp(): void
     {
@@ -23,11 +36,11 @@ class QueueableBulkAction extends \Filament\Tables\Actions\BulkAction
         $this
             ->successNotificationTitle('Bulk action queued successfully')
             ->action(function (Collection $records, QueueableBulkAction $action, array $data, BasePage $livewire) {
-                if (! $action->getJob()) {
+                if (!$action->getJob()) {
                     throw new Exception(QueueableBulkAction::class . ' requires a job to be set');
                 }
 
-                $bulkAction = $this->createBulkAction(
+                $bulkAction = $action->createBulkAction(
                     identifier: $livewire->getRenderHookScopes()[0],
                     totalRecords: $records->count(),
                     data: $data,
@@ -41,17 +54,17 @@ class QueueableBulkAction extends \Filament\Tables\Actions\BulkAction
     private function createBulkAction(string $identifier, int $totalRecords, array $data): BulkAction
     {
         return Config::bulkActionModel()::query()->create([
-            'name' => $this->getLabel(),
-            'type' => TypeEnum::TABLE,
-            'identifier' => $identifier,
-            'job' => $this->getJob(),
-            'user_id' => Auth::user()->getKey(),
+            'name'          => $this->getLabel(),
+            'type'          => TypeEnum::TABLE,
+            'identifier'    => $identifier,
+            'job'           => $this->getJob(),
+            'user_id'       => Auth::user()->getKey(),
             'total_records' => $totalRecords,
-            'data' => $data,
+            'data'          => $data,
         ]);
     }
 
-    public function job(Closure | string $job): static
+    public function job(Closure|string $job): static
     {
         $this->job = $job;
 
@@ -61,5 +74,29 @@ class QueueableBulkAction extends \Filament\Tables\Actions\BulkAction
     public function getJob(): ?string
     {
         return $this->evaluate($this->job);
+    }
+
+    public function __serialize(): array
+    {
+        return [
+            'name'                     => $this->name,
+            'action'                   => new SerializableClosure($this->action),
+            'livewire'                 => $this->getLivewire(),
+            'records'                  => $this->records,
+            'job'                      => $this->job,
+            'successNotificationTitle' => $this->successNotificationTitle,
+            'failureNotificationTitle' => $this->failureNotificationTitle,
+        ];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->name = $data['name'];
+        $this->action = $data['action']->getClosure();
+        $this->livewire = $data['livewire'];
+        $this->records = $data['records'];
+        $this->job = $data['job'];
+        $this->successNotificationTitle = $data['successNotificationTitle'];
+        $this->failureNotificationTitle = $data['failureNotificationTitle'];
     }
 }
